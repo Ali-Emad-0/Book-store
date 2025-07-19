22fddb39dce3ee2b7246674c4f8c5170fbb81493
Ali Emad <aliemad43210@gmail.com>
2025-07-19 19:28:19 +0300

    The static, templates, and redirect settings have been handled

---
+++ Book_Store/static/js/Home Page Script.js
@@ -0,0 +1,5 @@
+function scrollCarousel(button, direction) {
+  const container = button.parentElement.querySelector(".book-carousel");
+  const scrollAmount = 900;
+  container.scrollBy({ left: direction * scrollAmount, behavior: "smooth" });
+}
