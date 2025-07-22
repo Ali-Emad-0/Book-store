fd9bd73f72a826cce36488007f3b0d6b83458b08
Elbehwashi0 <abdullah.mohamed.9511@gmail.com>
2025-07-22 18:06:31 +0300

    Add Book and Category models

--- Main/models.py
+++ Main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    description = models.TextField()
+    category = models.ForeignKey("Category", on_delete=models.CASCADE, related_name="books", default=1)
+    image = models.ImageField(upload_to='books/%y/%m/%d/')
+    is_new = models.BooleanField(default=False)
+    is_popular = models.BooleanField(default=False)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
